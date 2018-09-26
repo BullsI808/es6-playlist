8bf76d84cc8304749a51a374d1b69fe5afe29c88
BullsI808 <JBasuil8697@myhta.org>
2018-09-26 12:23:58 -1000

    added data for pop and electronic

---
+++ data.js
@@ -0,0 +1,111 @@
+const data = [
+    {
+        artist: 'shapov & trouze',
+        genre: 'electronic',
+        song: 'the way',
+        image: 'http://weraveyou.com/wp-content/uploads/2018/01/desktop_2560x1440.png'
+    },
+    {
+        artist: 's3rl',
+        genre: 'electronic',
+        song: 'its this again',
+        image: 'https://i.ytimg.com/vi/ASW38HVbZls/maxresdefault.jpg'
+    },
+    {
+        artist: 's3rl',
+        genre: 'electronic',
+        song: 'beat all the odds',
+        image: 'http://weraveyou.com/wp-content/uploads/2018/01/desktop_2560x1440.png'
+    },
+    {
+        artist: 'masara',
+        genre: 'electronic',
+        song: 'tegami',
+        image: 'https://i.ytimg.com/vi/ZIu1ec_VDys/maxresdefault.jpg'
+    },
+    {
+        artist: 'dark cat',
+        genre: 'electronic',
+        song: 'bubble tea',
+        image: 'http://weraveyou.com/wp-content/uploads/2018/01/desktop_2560x1440.png'
+    },
+    {
+        artist: 'marshmello',
+        genre: 'electronic',
+        song: 'alone',
+        image: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/bd/Alone_Marshmello_cover.jpg/220px-Alone_Marshmello_cover.jpg'
+    },
+    {
+        artist: 'joji',
+        genre: 'rap', 
+        song: 'head in the clouds',
+        image: 'https://i.ytimg.com/vi/CX4fG9jLz_o/maxresdefault.jpg'
+    },
+    {
+        artist: '',
+        genre: 'rap', 
+        song: '',
+        image: ''
+    },
+    {
+        artist: '',
+        genre: 'rap', 
+        song: '',
+        image: ''
+    },
+    {
+        artist: '',
+        genre: 'rap', 
+        song: '',
+        image: ''
+    },
+    {
+        artist: '',
+        genre: 'rap', 
+        song: '',
+        image: ''
+    },
+    {
+        artist: '',
+        genre: 'rap', 
+        song: '',
+        image: ''
+    },
+    {
+        artist: 'owl city',
+        genre: 'pop', 
+        song: 'when can i see you again',
+        image: 'https://i.ytimg.com/vi/fpnUrPUlJXY/maxresdefault.jpg'
+    },
+    {
+        artist: 'a-ha',
+        genre: 'pop', 
+        song: 'take on me',
+        image: 'https://i.ytimg.com/vi/djV11Xbc914/hqdefault.jpg'
+    },
+    {
+        artist: 'owl city',
+        genre: 'pop', 
+        song: 'fireflies',
+        image: 'https://i.ytimg.com/vi/psuRGfAaju4/maxresdefault.jpg'
+    },
+    {
+        artist: 'ed sheeran',
+        genre: 'pop', 
+        song: 'shape of you',
+        image: 'https://i.ytimg.com/vi/1dv7iGA4hQw/maxresdefault.jpg'
+    },
+    {
+        artist: 'justin bieber',
+        genre: 'pop', 
+        song: 'children',
+        image: ''
+    },
+    {
+        artist: 'bruno mars',
+        genre: 'pop', 
+        song: '24k magic',
+        image: 'https://images-na.ssl-images-amazon.com/images/I/41NKk2fHW6L._SY355_.jpg'
+    },
+    
+]
