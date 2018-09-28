64b5f498b6ab6e57e9f4397a1a12e9f50ac72f25
BullsI808 <JBasuil8697@myhta.org>
2018-09-27 16:57:28 -1000

    added a video and a link to each song listed so far

---
+++ app.js
@@ -0,0 +1,37 @@
+console.log(data);
+const result1 = document.querySelector('#result1');
+const result2 = document.querySelector('#result2');
+
+const electronic = data.filter(song =>{
+    return song.genre === 'electronic';
+})
+.map(song =>{
+    return `
+    <div>  ${song.artist} <br> ${song.genre} <br> ${song.song} </div>
+    <img src = ${song.image}>
+    <br>
+    <a href = ${song.video}>song link </a>
+    <br>
+    <br>
+    `;
+})
+.join('');
+
+const pop = data.filter(song =>{
+    return song.genre === 'pop';
+})
+.map(song =>{
+    return `
+    <div>  ${song.artist} <br> ${song.genre} <br> ${song.song} </div>
+    <img src = ${song.image}>
+    <br>
+    <a href = ${song.video}>song link </a>
+    <br>
+    <br>
+    `;
+})
+.join('');
+
+result2.innerHTML = pop;
+result1.innerHTML = electronic;
+//console.log('electronic', data.electronic);
--- data.js
+++ data.js
@@ -3,37 +3,43 @@ const data = [
         artist: 'shapov & trouze',
         genre: 'electronic',
         song: 'the way',
-        image: 'http://weraveyou.com/wp-content/uploads/2018/01/desktop_2560x1440.png'
+        image: 'http://weraveyou.com/wp-content/uploads/2018/01/desktop_2560x1440.png',
+        video:'https://www.youtube.com/watch?v=1FCCpO0er44'
     },
     {
         artist: 's3rl',
         genre: 'electronic',
         song: 'its this again',
-        image: 'https://i.ytimg.com/vi/ASW38HVbZls/maxresdefault.jpg'
+        image: 'https://i.ytimg.com/vi/ASW38HVbZls/maxresdefault.jpg',
+        video: 'https://www.youtube.com/watch?v=ASW38HVbZls'
     },
     {
         artist: 's3rl',
         genre: 'electronic',
         song: 'beat all the odds',
-        image: 'http://weraveyou.com/wp-content/uploads/2018/01/desktop_2560x1440.png'
+        image: 'https://i.ytimg.com/vi/2P_KqFUEsDc/maxresdefault.jpg',
+        video: 'https://www.youtube.com/watch?v=2P_KqFUEsDc'
     },
     {
         artist: 'masara',
         genre: 'electronic',
         song: 'tegami',
-        image: 'https://i.ytimg.com/vi/ZIu1ec_VDys/maxresdefault.jpg'
+        image: 'https://i.ytimg.com/vi/ZIu1ec_VDys/maxresdefault.jpg',
+        video: 'https://www.youtube.com/watch?v=ZIu1ec_VDys'
     },
     {
         artist: 'dark cat',
         genre: 'electronic',
         song: 'bubble tea',
-        image: 'http://weraveyou.com/wp-content/uploads/2018/01/desktop_2560x1440.png'
+        image: 'https://i.ytimg.com/vi/7PYe57MwxPI/maxresdefault.jpg',
+        video: 'https://www.youtube.com/watch?v=7PYe57MwxPI'
     },
     {
         artist: 'marshmello',
         genre: 'electronic',
         song: 'alone',
-        image: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/bd/Alone_Marshmello_cover.jpg/220px-Alone_Marshmello_cover.jpg'
+        image: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/bd/Alone_Marshmello_cover.jpg/220px-Alone_Marshmello_cover.jpg',
+        video: 'https://www.youtube.com/watch?v=ALZHF5UqnU4'
     },
     {
         artist: 'joji',
@@ -75,37 +81,43 @@ const data = [
         artist: 'owl city',
         genre: 'pop', 
         song: 'when can i see you again',
-        image: 'https://i.ytimg.com/vi/fpnUrPUlJXY/maxresdefault.jpg'
+        image: 'https://i.ytimg.com/vi/fpnUrPUlJXY/maxresdefault.jpg',
+        video:'https://www.youtube.com/watch?v=fpnUrPUlJXY'
     },
     {
         artist: 'a-ha',
         genre: 'pop', 
         song: 'take on me',
-        image: 'https://i.ytimg.com/vi/djV11Xbc914/hqdefault.jpg'
+        image: 'https://i.ytimg.com/vi/djV11Xbc914/hqdefault.jpg',
+        video:'https://www.youtube.com/watch?v=djV11Xbc914'
     },
     {
         artist: 'owl city',
         genre: 'pop', 
         song: 'fireflies',
-        image: 'https://i.ytimg.com/vi/psuRGfAaju4/maxresdefault.jpg'
+        image: 'https://i.ytimg.com/vi/psuRGfAaju4/maxresdefault.jpg',
+        video:'https://www.youtube.com/watch?v=psuRGfAaju4'
     },
     {
         artist: 'ed sheeran',
         genre: 'pop', 
         song: 'shape of you',
-        image: 'https://i.ytimg.com/vi/1dv7iGA4hQw/maxresdefault.jpg'
+        image: 'https://i.ytimg.com/vi/1dv7iGA4hQw/maxresdefault.jpg',
+        video:'https://www.youtube.com/watch?v=JGwWNGJdvx8'
     },
     {
         artist: 'justin bieber',
         genre: 'pop', 
         song: 'children',
-        image: ''
+        image: 'https://i.ytimg.com/vi/nkjwn240AMc/maxresdefault.jpg',
+        video:'https://www.youtube.com/watch?v=nkjwn240AMc'
     },
     {
         artist: 'bruno mars',
         genre: 'pop', 
         song: '24k magic',
-        image: 'https://images-na.ssl-images-amazon.com/images/I/41NKk2fHW6L._SY355_.jpg'
+        image: 'https://images-na.ssl-images-amazon.com/images/I/41NKk2fHW6L._SY355_.jpg',
+        video:'https://www.youtube.com/watch?v=UqyT8IEBkvY'
     },
     
-]
+];
